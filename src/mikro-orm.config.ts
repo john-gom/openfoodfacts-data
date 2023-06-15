29c40bad95dc03ee784a93120e6cacc5473641a2
John Gomersall <thegoms@gmail.com>
2023-06-15 14:57:58 +0100

    Basic NestJS Mikro-orm project

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,9 @@
+export default {
+  entities: ['./dist/entities'],
+  entitiesTs: ['./src/entities'],
+  dbName: 'postgres',
+  user: 'postgres',
+  password: 'cactus789',
+  schema: 'off',
+  type: 'postgresql',
+};
